b823a64e0257591da84e3786b97170e4be96d173
youngers2006 <sy2224@ic.ac.uk>
2026-08-14 12:52:40 +0100

    slurm

---
+++ slurm_scripts/Test_Train_Job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -A Research_Project-DeepMind
+#SBATCH --time=23:00:00
+#SBATCH --nodes=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sy493@exeter.ac.uk
+#SBATCH --gres=gpu:1               
+#SBATCH --cpus-per-task=8          
+#SBATCH --mem=32G                  
+#SBATCH --output=train_%j.out
+#SBATCH --error=train_%j.err
+
+cd /lustre/home/sy493/Safe_JEPA/safe_wm
+
+module load cuda/12.1
+
+export WANDB_API_KEY="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"
+
+/lustre/home/sy493/.conda/envs/safe_JEPA/bin/python training_point_maze.py
